ab10c70295bd7b4829c32fb8de9e9a6a13cfcd4d
Máté Marjai <mate@theproduct.works>
2018-11-24 06:30:52 +0000

    Simple creature implementation - only holds skill and stamina

---
+++ lib/adventurex/creature.ex
@@ -0,0 +1,27 @@
+defmodule Adventurex.Creature do
+  use GenServer
+
+  alias Adventurex.Dice
+
+  # Attributes is a map with skill, stamina
+  def start_link(name, attributes) do
+    GenServer.start_link(__MODULE__, %{attributes | name: name}, name: {:global, name})
+  end
+
+  def skill(pid), do: GenServer.call({:global, pid}, {:get, :skill})
+  def stamina(pid), do: GenServer.call({:global, pid}, {:get, :stamina})
+
+  def skill(pid, change), do: GenServer.call({:global, pid}, {:put, :skill, change})
+  def stamina(pid, change), do: GenServer.call({:global, pid}, {:put, :stamina, change})
+
+  # Simple init - just go with the state received
+  def init(state), do: {:ok, state}
+
+  def handle_call({:get, attribute}, _from, state) do
+    {:reply, state[:attribute], state}
+  end
+
+  def handle_cast({:put, attribute, change}, state) do
+    {:noreply, Map.update!(state, attribute, &(&1 + change))}
+  end
+end
